4876cfb069d617439ff0b83b827ee055dd56f16b
Antoine <antoine@crafting-labs.fr>
2016-02-15 16:03:34 +0100

    Add a font with 9 lines height
    A big thanks to @vferries for that gist https://gist.github.com/avernois/9fc2c459ed7802570c37
    
    and use it in in SlidingHelloWorldExample

--- ledmatrix-examples/src/main/java/fr/craftinglabs/pi/matrix/examples/SlidingHelloWorldExample.java
+++ ledmatrix-examples/src/main/java/fr/craftinglabs/pi/matrix/examples/SlidingHelloWorldExample.java
@@ -2,14 +2,15 @@ package fr.craftinglabs.pi.matrix.examples;
 
 import fr.craftinglabs.pi.matrix.Matrix;
 import fr.craftinglabs.pi.matrix.Size;
+import fr.craftinglabs.pi.matrix.font.Font9;
 import fr.craftinglabs.pi.matrix.frame.SlidingGlyphs;
-import fr.craftinglabs.pi.matrix.font.Font6;
 
 public class SlidingHelloWorldExample {
  
     public static void main(String[] args) throws InterruptedException {
-        Matrix matrix = new Matrix(new Size(6, 8));
+        Matrix matrix = new Matrix(new Size(9, 32));
 
-        matrix.print(10, new SlidingGlyphs(new Size(6, 8), Font6.H, Font6.e, Font6.l, Font6.l, Font6.o, Font6.SPACE, Font6.W, Font6.o, Font6.r, Font6.l, Font6.d, Font6.MARK));
+        matrix.print(300, new SlidingGlyphs(new Size(9, 32), Font9.H, Font9.e, Font9.l, Font9.l, Font9.o, Font9.SPACE, Font9.W, Font9.o, Font9.r, Font9.l, Font9.d, Font9.EXCLAMATION));
+        matrix.print(300, new SlidingGlyphs(new Size(9, 32), Font9.B, Font9.o, Font9.n, Font9.j, Font9.o, Font9.u, Font9.r, Font9.SPACE, Font9.l, Font9.e, Font9.SPACE, Font9.m, Font9.o, Font9.n, Font9.d, Font9.e, Font9.EXCLAMATION ));
     }
 }
