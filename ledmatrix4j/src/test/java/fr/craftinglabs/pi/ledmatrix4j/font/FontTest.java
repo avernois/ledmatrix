6e0339d5e1336a1dd20ca046c3fdd5a7815524d6
Antoine <antoine@crafting-labs.fr>
2016-03-10 12:24:00 +0100

    Refactoring of Font system
    Font is now a simple list of Glyph, and can return the glyph corresponding to a give char.
    
    Font6 and Font9 do not inherited of Font anymore (which was odd from the start). Instead, they build a font and offer a method to get it.
    
    Maybe this method should be an interface. I'm not sure it really matter yet.

--- ledmatrix4j/src/test/java/fr/craftinglabs/pi/ledmatrix4j/font/FontTest.java
+++ ledmatrix4j/src/test/java/fr/craftinglabs/pi/ledmatrix4j/font/FontTest.java
@@ -1,20 +1,23 @@
 package fr.craftinglabs.pi.ledmatrix4j.font;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class FontTest {
 
     @Test
     public void should_be_made_of_glyphs() {
-        Font font = new Font();
         Glyph g1 = new Glyph('1',
                 "0010",
                 "0010",
                 "0010",
                 "0010");
-        Font.add(g1);
 
-        assertEquals(g1, Font.glyphFor('1'));
+        Font font = new Font(Arrays.asList(g1));
+
+        assertEquals(g1, font.glyphFor('1'));
     }
 }
