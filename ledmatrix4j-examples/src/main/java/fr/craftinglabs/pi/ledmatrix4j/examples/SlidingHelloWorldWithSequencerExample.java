460c38a661b9b448df7077cef4eb9801af259829
fpellet <florent.pellet@gmail.com>
2016-06-18 13:57:23 +0200

    Refactor SlidingHelloWorldWithSequencerExample

--- ledmatrix4j-examples/src/main/java/fr/craftinglabs/pi/ledmatrix4j/examples/SlidingHelloWorldWithSequencerExample.java
+++ ledmatrix4j-examples/src/main/java/fr/craftinglabs/pi/ledmatrix4j/examples/SlidingHelloWorldWithSequencerExample.java
@@ -3,6 +3,7 @@ package fr.craftinglabs.pi.ledmatrix4j.examples;
 import fr.craftinglabs.pi.ledmatrix4j.Matrix;
 import fr.craftinglabs.pi.ledmatrix4j.Size;
 import fr.craftinglabs.pi.ledmatrix4j.font.Font9;
+import fr.craftinglabs.pi.ledmatrix4j.font.Glyph;
 import fr.craftinglabs.pi.ledmatrix4j.frame.LineFrameSequencer;
 import fr.craftinglabs.pi.ledmatrix4j.frame.LineFrameSequencerIterable;
 import fr.craftinglabs.pi.ledmatrix4j.frame.SlidingGlyphs;
@@ -16,10 +17,18 @@ public class SlidingHelloWorldWithSequencerExample {
  
     public static void main(String[] args) throws InterruptedException {
         Matrix matrix = new Matrix(new Size(9, 32));
+        Duration delayByFrame = Duration.ofSeconds(40);
 
-        SlidingGlyphs hello_world = new SlidingGlyphs(new Size(9, 32), Arrays.asList(H, e, l, l, o, SPACE, W, o, r, l, d, EXCLAMATION));
-        matrix.print(Duration.ofSeconds(40), new LineFrameSequencerIterable(hello_world.iterator(), new LineFrameSequencer()));
-        SlidingGlyphs bonjour_monde = new SlidingGlyphs(new Size(9, 32), Arrays.asList(B, o, n, j, o, u, r, SPACE, l, e, SPACE, m, o, n, d, e, EXCLAMATION ));
-        matrix.print(Duration.ofSeconds(40), new LineFrameSequencerIterable(bonjour_monde.iterator(), new LineFrameSequencer()));
+        matrix.print(delayByFrame, format(H, e, l, l, o, SPACE, W, o, r, l, d, EXCLAMATION));
+        matrix.print(delayByFrame, format(B, o, n, j, o, u, r, SPACE, l, e, SPACE, m, o, n, d, e, EXCLAMATION));
+    }
+
+    private static LineFrameSequencerIterable format(Glyph... characters){
+        SlidingGlyphs glyphs = new SlidingGlyphs(
+                new Size(9, 32),
+                Arrays.asList(characters));
+        return new LineFrameSequencerIterable(
+                glyphs.iterator(),
+                new LineFrameSequencer());
     }
 }
